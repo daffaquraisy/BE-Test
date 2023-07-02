c74b17af81b412c791a8890971260ee2d5f10e78
daffaquraisy <41823602+daffaquraisy@users.noreply.github.com>
2023-07-02 15:12:10 +0700

    Update exampleController.js

--- app/controllers/exampleController.js
+++ app/controllers/exampleController.js
@@ -1,7 +1,10 @@
-let data []
+const dataLatLon = require("./files/latlon.js");
+
+let data = dataLatLon
 
 exports.callmeWebSocket = (req, res) => {
   // endpoint livethreat tidak return latlon
   // jadi kamu harus melookup latlon tersebut 
   // berdasarkan destinationCountry and sourceCountry yang berada di variable data 
+  console.log(dataLatLon)
 };
